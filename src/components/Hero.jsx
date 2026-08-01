04da13c84e6663845413677a1a18347812f6e990
alejo-software-lab <alejosoftwarelabs@gmail.com>
2026-08-01 12:56:57 -0500

    Aumenta logo en Hero a h-48

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
                <img
                  src="/logo.png"
                  alt="Alejo Software Labs"
-                 className="h-36 w-auto"
+                 className="h-48 w-auto"
                />
                <span className="text-5xl font-extrabold tracking-tight text-slate-900 dark:text-white">
                  Alejo Software Labs
